7d3723694c59a27371cb3626800712fef22b41e5
sfazleev <fazleevse@gmail.com>
2024-02-21 17:26:41 +0400

    add1

---
+++ src/test/java/example/WebTest.java
@@ -0,0 +1,46 @@
+package example;
+
+
+import example.pages.ConfProperties;
+import example.pages.WebElementsPage;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.time.Duration;
+
+
+public class WebTest {
+
+    public static WebElementsPage webElementsPage;
+    public static WebDriver driver;
+
+    @BeforeAll
+    public static void open() {
+        System.setProperty("webdriver.chrome.driver", ConfProperties.getProperty("chromedriver"));
+        driver = new ChromeDriver();
+        webElementsPage = new WebElementsPage(driver);
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+        driver.get(ConfProperties.getProperty("homepage"));
+    }
+
+
+    @Test
+    public void webest1() {
+
+
+
+ }
+
+
+    @AfterAll
+    public static void close() {
+        driver.quit(); }
+
+
+
+}
+
